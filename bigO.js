536afe129feb55df557074a957b2244df361e945
Yvonne Chen <yvonnechen1991@gmail.com>
2021-05-27 15:03:14 -0500

    problem patterns

--- bigO.js
+++ bigO.js
@@ -2,67 +2,67 @@
 // Suppose we want to write a function that calculates the sum of all numbers from 1 up to (and including) some number n.
 
 function addUpTo(n) {
-    let total = 0;
-    for (let i = 1; i <= n; i++) {
-      total += i;
-    }
-    return total;
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
   }
+  return total;
+}
 
-//   3 simple operations, regardless of the size of n
-  function addUpTo(n) {
-    return n * (n + 1) / 2;
-  }
+// 3 simple operations, regardless of the size of n
+function addUpTo(n) {
+  return (n * (n + 1)) / 2;
+}
 
-  function addUpTo(n) {
-    let total = 0;
-    for (let i = 1; i <= n; i++) {
-      total += i;
-    }
-    return total;
+function addUpTo(n) {
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
   }
-  
-  let t1 = performance.now();
-  addUpTo(1000000000);
-  let t2 = performance.now();
-  console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
+  return total;
+}
 
-//   Number of operations is (eventually) bounded by a multiple of n (say, 10n) => O(n)
-  function countUpAndDown(n) {
-    console.log("Going up!");
-    for (let i = 0; i < n; i++) {
-      console.log(i);
-    }
-    console.log("At the top!\nGoing down...");
-    for (let j = n - 1; j >= 0; j--) {
-      console.log(j);
-    }
-    console.log("Back down. Bye!");
+let t1 = performance.now();
+addUpTo(1000000000);
+let t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
+
+// Number of operations is (eventually) bounded by a multiple of n (say, 10n) => O(n)
+function countUpAndDown(n) {
+  console.log("Going up!");
+  for (let i = 0; i < n; i++) {
+    console.log(i);
   }
+  console.log("At the top!\nGoing down...");
+  for (let j = n - 1; j >= 0; j--) {
+    console.log(j);
+  }
+  console.log("Back down. Bye!");
+}
 
 //   O(n) operation inside of an O(n) operation => O(n * n) => O(n^2)
-  function printAllPairs(n) {
-    for (var i = 0; i < n; i++) {
-      for (var j = 0; j < n; j++) {
-        console.log(i, j);
-      }
+function printAllPairs(n) {
+  for (var i = 0; i < n; i++) {
+    for (var j = 0; j < n; j++) {
+      console.log(i, j);
     }
   }
+}
 
-  // O(1) space
-  function sum(arr) {
-    let total = 0;
-    for (let i = 0; i < arr.length; i++) {
-      total += arr[i];
-    }
-    return total;
+// O(1) space
+function sum(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
   }
+  return total;
+}
 
-  // O(n) space
-  function double(arr) {
-    let newArr = [];
-    for (let i = 0; i < arr.length; i++) {
-      newArr.push(2 * arr[i]);
-    }
-    return newArr;
+// O(n) space
+function double(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(2 * arr[i]);
   }
+  return newArr;
+}
