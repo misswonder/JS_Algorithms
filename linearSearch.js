a44ff2895a0c05be2f94efed194a915fd0db6b52
Yvonne Chen <yvonnechen1991@gmail.com>
2021-06-03 00:34:37 -0500

    search part 1

---
+++ linearSearch.js
@@ -0,0 +1,10 @@
+// Linear Search
+// .indexOf(), .find(), .findIndex(), includes()
+// o(n)
+function linearSearch(arr, val) {
+  // add whatever parameters you deem necessary - good luck!
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) return i;
+  }
+  return -1;
+}
