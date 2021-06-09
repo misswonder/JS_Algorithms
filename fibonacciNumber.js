62649482f1c49ea84d1b581701acb5bb0d9dfc01
Yvonne Chen <yvonnechen1991@gmail.com>
2021-06-09 18:39:23 -0500

    quick sort

---
+++ fibonacciNumber.js
@@ -0,0 +1,16 @@
+var fib = function (n) {
+  if (n === 0 || n === 1) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+};
+
+// FIBONACCI SOLUTION
+// fib(4) // 3
+// fib(10) // 55
+// fib(28) // 317811
+// fib(35) // 9227465
+function fib(n) {
+  if (n <= 2) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
--- recursion_challenges_1.js
+++ recursion_challenges_1.js
@@ -46,3 +46,4 @@ function fib(n) {
   if (n <= 2) return 1;
   return fib(n - 1) + fib(n - 2);
 }
+
